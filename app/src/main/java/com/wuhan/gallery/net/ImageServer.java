820fa24f7b27d526f1bc5d9da69259f092e8c45b
liligang <lgzc_work@163.com>
2018-04-20 16:00:00 +0800

    首页获取网络数据

--- app/src/main/java/com/wuhan/gallery/net/ImageServer.java
+++ app/src/main/java/com/wuhan/gallery/net/ImageServer.java
@@ -1,14 +1,10 @@
 package com.wuhan.gallery.net;
 
 import com.wuhan.gallery.bean.HostDataBean;
-import com.wuhan.gallery.bean.ImageBean;
 import com.wuhan.gallery.bean.NetworkDataBean;
-
-import java.util.List;
 
 import io.reactivex.Observable;
 import retrofit2.http.GET;
-import retrofit2.http.Query;
 
 public interface ImageServer {
 
